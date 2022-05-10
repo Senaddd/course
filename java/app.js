a90ab90602b7ef6692e095a59284300da9a11236
Senad Djerlek <senajerlek@gmail.com>
2022-05-10 23:03:59 +0200

    javascript practice

--- java/app.js
+++ java/app.js
@@ -1,6 +1,23 @@
-let greetingText = "Hi my name is Max"
-alert(greetingText);
-alert(greetingText);
-greetingText = 'Hi,I am really Max!'
-alert (greetingText)
-alert (32);
+// let ageNumber = 32;
+// alert (ageNumber);
+// let greetingText = "Hi my name is Max"
+// alert(greetingText);
+// alert(greetingText);
+// greetingText = 'Hi,I am really Max!';
+// alert (greetingText);
+// alert (age);
+// alert (age);
+// let jobPlace = 'New York'
+// let jobSalary = 5000;
+// let jobTitle = 'Developer'
+
+let age = 32;
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
+};
+
+alert(hobbies[0]);
