8c68aa3a8200ccda1b32080cd94b1a6a230127c2
Senad Djerlek <senajerlek@gmail.com>
2022-05-24 00:13:19 +0200

    javascript

--- java/operations.js
+++ java/operations.js
@@ -6,3 +6,25 @@ console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4); // 10 / 4 = 2 * 4 = Remainder: 2
 console.log(10 % 3); // 1
+
+console.log((10 + 3 - 5) * 10);
+
+// let result = ((10 + 3 - 5) * 10);
+// result = 10 * 4;
+// result++; // result = result + 1
+//         //result -- 1 = result -1
+
+//  result += 5; // reulst = result +5; //also exsist for minus and devision and multiple occasions
+//  result -= 5;
+//  result /= 5;
+//  result *= 5;
+//  console.log(result); 
+console.log("Max" + "Schwarzmuller");
+console.log("Max" - "ax");
+
+let userName = "Max";
+console.log(userName.length);
+console.log(userName.toUpperCase());
+
+let hobbies = ["Sports", "Cooking"];
+console.log([hobbies.length]);
